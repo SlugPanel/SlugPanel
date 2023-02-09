e3b12c94ba07ec7cc57bc011dba1e94d4f64900b
freeslugga <jjf1019@gmail.com>
2023-02-09 06:10:52 -0500

    6:10 am

--- api/server.js
+++ api/server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 const usar_db = mongoose.createConnection("mongodb://usarAdmin:poopusar@slug-db:27017/UsarData")
-    .then(() => console.log("connected"))
 
 
 const User = usar_db.model('User', userSchema)
