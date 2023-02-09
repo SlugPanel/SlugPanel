d6d5bb3d79cadac1a48750ee0c54334886597850
freeslugga <jjf1019@gmail.com>
2023-02-09 06:25:06 -0500

    giving up soon

--- api/server.js
+++ api/server.js
@@ -13,7 +13,13 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const usar_db = mongoose.createConnection("mongodb://usarAdmin:poopusar@slug-db:27017/UsarData", {auth: {authdb: 'admin'}})
+const usar_db = mongoose.createConnection("mongodb://slug-db:27017/UsarData", {
+    auth: {
+        username: "usarAdmin",
+        password: "poopusar",
+        authdb: "admin"
+    },
+});
 
 
 const User = usar_db.model('User', userSchema)
