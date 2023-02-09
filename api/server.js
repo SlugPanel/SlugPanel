6802a2fcd94d61f2420b55a53755cd6138cf0bf0
freeslugga <jjf1019@gmail.com>
2023-02-09 04:59:25 -0500

    I am up too late for this shit

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const usar_db = mongoose.createConnection("mongodb+srv://usar:i0dZ59pvJ5aFM190@slug-panel.b8jgn4x.mongodb.net/UsarData?retryWrites=true&w=majority")
+const usar_db = mongoose.createConnection("mongodb://"+process.env.username+":"+process.env.password+"@slug-panel.b8jgn4x.mongodb.net/UsarData?retryWrites=true&w=majority")
 
 
 const User = usar_db.model('User', userSchema)
