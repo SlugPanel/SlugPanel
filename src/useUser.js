2ef0fb4aa51c5bfea4855ab9aa79192bee78955b
freeslugga <jjf1019@gmail.com>
2023-02-10 12:46:00 -0500

    fix last commit

--- src/useUser.js
+++ src/useUser.js
@@ -2,18 +2,18 @@ import { useState } from 'react'
 
 export default function useUser() {
     const getUser = () => {
-        return sessionStorage.getItem('user')
-    }
+        return sessionStorage.getItem('user');
+    };
 
     const [user, setUser] = useState(getUser);
 
     const saveUser = userobject => {
-        sessionStorage.setItem('user', userobject.user)
-        setUser(userobject.user)
-    }
+        sessionStorage.setItem('user', userobject.user);
+        setUser(userobject.user);
+    };
 
     return {
-        setUser: saveUser(),
+        setUser: saveUser,
         user
     }
 }
